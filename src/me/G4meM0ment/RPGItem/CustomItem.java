013ad6f356b5e86cde89d88c9a0ea3945a972570
G4meM0ment <G4meM0ments@gmx.net>
2013-08-26 21:24:34 +0200

    rpgitem getters and setters

--- src/me/G4meM0ment/RPGItem/CustomItem.java
+++ src/me/G4meM0ment/RPGItem/CustomItem.java
@@ -1,11 +1,15 @@
 package me.G4meM0ment.RPGItem;
 
-public class CustomItem {
+import org.bukkit.inventory.ItemStack;
+
+public class CustomItem extends ItemStack{
 	
 	private int skinId, dmgValue, defSkinId, defDmgValue;
 	private String dispName, desc, price, lore, defDesc, defPrice, defLore;
+	private ItemStack item;
 	
-	public CustomItem(String dispName, int skinId, int dmgValue, String desc, String price, String lore) {
+	public CustomItem(ItemStack item, String dispName, int skinId, int dmgValue, String desc, String price, String lore) {
+		this.item = item;
 		this.dispName = dispName;
 		this.skinId = skinId;
 		this.dmgValue = dmgValue;
@@ -14,7 +18,8 @@ public class CustomItem {
 		this.lore = lore;
 	}
 	
-	public CustomItem(String dispName) {
+	public CustomItem(ItemStack item, String dispName) {
+		this.item = item;
 		this.dispName = dispName;
 		skinId = defSkinId;
 		dmgValue = defDmgValue;
@@ -23,7 +28,40 @@ public class CustomItem {
 		lore = defLore;
 	}
 	
+	public ItemStack getItem() {
+		return item;
+	}
+	public void setItem(ItemStack item) {
+		this.item = item;
+	}
 	
+	public int getSkinId() {
+		return skinId;
+	}
+	public void setSkinId(int id) {
+		skinId = id;
+	}
+	
+	public int getDefSkinId() {
+		return defSkinId;
+	}
+	public void setDefSkinId(int id) {
+		defSkinId = id;
+	}
+	
+	public int getDmgValue() {
+		return dmgValue;
+	}
+	public void setDmgValue(int i) {
+		dmgValue = i;
+	}
+	
+	public int getDefDmgValue() {
+		return defDmgValue;
+	}
+	public void setDefDmgValue(int i) {
+		defDmgValue = i;
+	}
 
 	
 	
